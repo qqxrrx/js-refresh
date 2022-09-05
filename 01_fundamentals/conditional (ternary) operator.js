3aec46e1c2ba908a8bd6cd6706ff9e3b59cc549a
ascendxrr <112617626+ascendxrr@users.noreply.github.com>
2022-09-06 07:49:32 +0800

    conditional (ternary) operator

---
+++ 01_fundamentals/conditional (ternary) operator.js
@@ -0,0 +1,8 @@
+const age = 19
+age >= 18 ? console.log(`of full age`) : console.log(`still a child`)
+
+const sta = age >= 18 ? `legal age` : `minor`
+console.log(sta)
+
+const ans = false
+console.log(`the answer was: ${ans === false ? true : false}`)
