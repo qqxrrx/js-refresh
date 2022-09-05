1ecada45379392e5b701a2ff2c9aec38953475a0
ascendxrr <112617626+ascendxrr@users.noreply.github.com>
2022-09-06 07:39:29 +0800

    statements vs expressions

---
+++ 01_fundamentals/statements vs expressions.js
@@ -0,0 +1,18 @@
+3 + 2 // expression
+true // expression
+;('wow') // expression
+
+// if-else statement
+if (true) {
+  // do stuff
+}
+
+// switch statement
+const c = 0
+switch (c) {
+  case 'a':
+    // case 'a'
+    break
+  default:
+  // default
+}
