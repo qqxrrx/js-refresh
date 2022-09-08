872e9fa7bb278535347ebd0faec49342fea4c33b
ascendxrr <112617626+ascendxrr@users.noreply.github.com>
2022-09-08 14:45:41 +0800

    introduction to objects

---
+++ 01B_fundamentals (part II)/objects.js
@@ -0,0 +1,14 @@
+'use strict'
+
+// object with 5 properties
+// object literal syntax
+// arrays used for ordered data, objects for unstructured data
+const john = {
+  firstName: 'john',
+  lastName: 'doe',
+  age: 2044 - 2002,
+  job: 'professor',
+  friends: ['A', 'B', 'C'],
+}
+
+console.log(john)
