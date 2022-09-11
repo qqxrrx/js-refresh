3e1524cc9e104c90f80539f6d61a29be9082dc6d
ascendxrr <112617626+ascendxrr@users.noreply.github.com>
2022-09-11 19:02:44 +0800

    handling click events

--- 02_dom/guess-my-num/script.js
+++ 02_dom/guess-my-num/script.js
@@ -1,15 +1,25 @@
 'use strict'
 
-console.log(document.querySelector('.message').textContent)
+// console.log(document.querySelector('.message').textContent)
 
-document.querySelector('.message').textContent = 'Correct Number!'
+// document.querySelector('.message').textContent = 'Correct Number!'
 
-console.log(document.querySelector('.message').textContent)
+// console.log(document.querySelector('.message').textContent)
 
-document.querySelector('.number').textContent = 13
+// document.querySelector('.number').textContent = 13
 
-document.querySelector('.score').textContent = 10
+// document.querySelector('.score').textContent = 10
 
-document.querySelector('.guess').value = 23
+// document.querySelector('.guess').value = 23
 
-console.log(document.querySelector('.guess').value)
+// console.log(document.querySelector('.guess').value)
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value)
+  console.log(guess, typeof guess)
+
+  // if falsy value, 0 or NaN is falsy
+  if (!guess) {
+    document.querySelector('.message').textContent = 'no number!'
+  }
+})
