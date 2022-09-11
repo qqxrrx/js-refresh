754f2a0138fa2ed8a16c127db0da8e2ed5248b47
ascendxrr <112617626+ascendxrr@users.noreply.github.com>
2022-09-12 04:59:56 +0800

    manipulating css style

--- 02_dom/guess-my-num/script.js
+++ 02_dom/guess-my-num/script.js
@@ -17,6 +17,8 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = 'no number!'
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'correct number!'
+    document.querySelector('body').style.backgroundColor = '#60b347'
+    document.querySelector('.number').style.width = '30rem'
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'too high!'
