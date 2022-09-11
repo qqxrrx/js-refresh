6e620206cc83303f221e520700410483d2f94dc3
ascendxrr <112617626+ascendxrr@users.noreply.github.com>
2022-09-12 04:51:34 +0800

    implement game logic

--- 02_dom/guess-my-num/script.js
+++ 02_dom/guess-my-num/script.js
@@ -1,18 +1,12 @@
 'use strict'
 
-// console.log(document.querySelector('.message').textContent)
+// Math.random() * 20 <-- 0 to 19
+const secretNumber = Math.trunc(Math.random() * 20) + 1
+document.querySelector('.number').textContent = secretNumber
 
-// document.querySelector('.message').textContent = 'Correct Number!'
-
-// console.log(document.querySelector('.message').textContent)
-
-// document.querySelector('.number').textContent = 13
-
-// document.querySelector('.score').textContent = 10
-
-// document.querySelector('.guess').value = 23
-
-// console.log(document.querySelector('.guess').value)
+// state variable, part of state of application
+let score = 20
+document.querySelector('.score').textContent = score
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value)
@@ -21,5 +15,31 @@ document.querySelector('.check').addEventListener('click', function () {
   // if falsy value, 0 or NaN is falsy
   if (!guess) {
     document.querySelector('.message').textContent = 'no number!'
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'correct number!'
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'too high!'
+      score--
+      document.querySelector('.score').textContent = score
+    } else {
+      document.querySelector('.message').textContent = 'you lost the game!'
+      score--
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'too low!'
+      score--
+      document.querySelector('.score').textContent = score
+    } else {
+      document.querySelector('.message').textContent = 'you lost the game!'
+      score--
+    }
+  }
+
+  if (score === 0) {
+    document.querySelector('.message').textContent = 'you lost the game!'
+    document.querySelector('.score').textContent = score
+    alert('game over!')
   }
 })
