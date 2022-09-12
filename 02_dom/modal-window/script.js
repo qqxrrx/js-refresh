2b42e2c75c46ed1d67302cf7f43f75853f839464
ascendxrr <112617626+ascendxrr@users.noreply.github.com>
2022-09-12 22:32:12 +0800

    working with classes

--- 02_dom/modal-window/script.js
+++ 02_dom/modal-window/script.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const modal = document.querySelector('modal')
+const modal = document.querySelector('.modal')
 const overlay = document.querySelector('.overlay')
 const btnCloseModal = document.querySelector('.close-modal')
 // select multiple elements of same css class, return NodeList which is like an array
@@ -12,6 +12,18 @@ console.log(btnsOpenModal)
 //   console.log(btnsOpenModal[i].textContent)
 // }
 
-btnsOpenModal.forEach((e) => {
-  console.log(e.textContent)
-})
+const openModal = function () {
+  console.log(this.textContent)
+  modal.classList.remove('hidden')
+  overlay.classList.remove('hidden')
+}
+
+const closeModal = function () {
+  modal.classList.add('hidden')
+  overlay.classList.add('hidden')
+}
+
+btnsOpenModal.forEach((e) => e.addEventListener('click', openModal))
+
+btnCloseModal.addEventListener('click', closeModal)
+overlay.addEventListener('click', closeModal)
