bb2c80824f77be80cf0235a8f497b693e553a466
ascendxrr <112617626+ascendxrr@users.noreply.github.com>
2022-09-12 19:44:58 +0800

    implement highscore

--- 02_dom/guess-my-num/script.js
+++ 02_dom/guess-my-num/script.js
@@ -7,6 +7,8 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1
 let score = 20
 document.querySelector('.score').textContent = score
 
+let highscore = 0
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value)
   console.log(guess, typeof guess)
@@ -19,6 +21,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').textContent = secretNumber
     document.querySelector('body').style.backgroundColor = '#60b347'
     document.querySelector('.number').style.width = '30rem'
+
+    if (score > highscore) {
+      highscore = score
+      document.querySelector('.highscore').textContent = highscore
+    }
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'too high!'
