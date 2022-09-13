f5869f5e984ffd2ec2f40a6c386706d4cf015c8e
ascendxrr <112617626+ascendxrr@users.noreply.github.com>
2022-09-13 19:30:15 +0800

    pig score (reset game)

--- 02_dom/pig-game/script2.js
+++ 02_dom/pig-game/script2.js
@@ -16,19 +16,30 @@ const btnRoll = document.querySelector('.btn--roll')
 const btnHold = document.querySelector('.btn--hold')
 
 // starting conditions
-score0El.textContent = 0
-score1El.textContent = 0
-diceEl.classList.add('hidden')
 
 // best practice = use state variable, persist value of fields instead of just relying on the dom
-let currentScore = 0
-// hold current active player
-// 0 = player 1, 1 = player 2
-let activePlayer = 0
+let scores, currentScore, activePlayer, playing
 
-let playing = true
+const init = function () {
+  // these variables decide starting datatype
+  scores = [0, 0]
+  currentScore = 0
+  activePlayer = 0
+  playing = true
 
-const scores = [0, 0]
+  score0El.textContent = 0
+  score1El.textContent = 0
+  current0El.textContent = 0
+  current1El.textContent = 0
+
+  diceEl.classList.add('hidden')
+  player0El.classList.remove('player--winner')
+  player1El.classList.remove('player--winner')
+  player0El.classList.add('player--active')
+  player1El.classList.remove('player--active')
+}
+
+init()
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0
@@ -86,3 +97,5 @@ btnHold.addEventListener('click', function () {
     }
   }
 })
+
+btnNew.addEventListener('click', init)
