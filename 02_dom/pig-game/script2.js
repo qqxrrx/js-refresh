820b3dea252ce127801b70ef286379e31a2ff843
ascendxrr <112617626+ascendxrr@users.noreply.github.com>
2022-09-13 11:11:36 +0800

    pig score (switch active player)

--- 02_dom/pig-game/script2.js
+++ 02_dom/pig-game/script2.js
@@ -3,6 +3,8 @@
 // script.js was my answer
 
 // selecting elements
+const player0El = document.querySelector('.player--0')
+const player1El = document.querySelector('.player--1')
 const score0El = document.querySelector('#score--0')
 const score1El = document.getElementById('score--1')
 const current0El = document.getElementById('current--0')
@@ -20,6 +22,11 @@ diceEl.classList.add('hidden')
 
 // best practice = use state variable, persist value of fields instead of just relying on the dom
 let currentScore = 0
+// hold current active player
+// 0 = player 1, 1 = player 2
+let activePlayer = 0
+
+const scores = [0, 0]
 
 // rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -35,8 +42,15 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // add dice to current score
     currentScore += dice
-    current0El.textContent = currentScore // change later
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore
   } else {
     // switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0
+    currentScore = 0
+    activePlayer = activePlayer === 0 ? 1 : 0
+    // .toggle() = add class if not there, remove class if exists
+    player0El.classList.toggle(`player--active`)
+    player1El.classList.toggle(`player--active`)
   }
 })
