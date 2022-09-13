daf552daaf630e5e3aadb433cc0b688f4928a621
ascendxrr <112617626+ascendxrr@users.noreply.github.com>
2022-09-13 11:37:58 +0800

    pig score (hold current score)

--- 02_dom/pig-game/script2.js
+++ 02_dom/pig-game/script2.js
@@ -26,31 +26,63 @@ let currentScore = 0
 // 0 = player 1, 1 = player 2
 let activePlayer = 0
 
+let playing = true
+
 const scores = [0, 0]
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0
+  currentScore = 0
+  activePlayer = activePlayer === 0 ? 1 : 0
+  // .toggle() = add class if not there, remove class if exists
+  player0El.classList.toggle(`player--active`)
+  player1El.classList.toggle(`player--active`)
+}
 
 // rolling dice functionality
 btnRoll.addEventListener('click', function () {
-  // 1. generate random dice roll
-  const dice = Math.trunc(Math.random() * 6) + 1
-  console.log(dice)
+  if (playing) {
+    // 1. generate random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1
 
-  // 2. display dice
-  diceEl.classList.remove('hidden')
-  diceEl.src = `dice-${dice}.png`
+    // 2. display dice
+    diceEl.classList.remove('hidden')
+    diceEl.src = `dice-${dice}.png`
 
-  // 3. check for rolled 1
-  if (dice !== 1) {
-    // add dice to current score
-    currentScore += dice
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore
-  } else {
-    // switch to next player
-    document.getElementById(`current--${activePlayer}`).textContent = 0
-    currentScore = 0
-    activePlayer = activePlayer === 0 ? 1 : 0
-    // .toggle() = add class if not there, remove class if exists
-    player0El.classList.toggle(`player--active`)
-    player1El.classList.toggle(`player--active`)
+    // 3. check for rolled 1
+    if (dice !== 1) {
+      // add dice to current score
+      currentScore += dice
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore
+    } else {
+      // switch to next player
+      switchPlayer()
+    }
+  }
+})
+
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    // 1. add current score to active player's score
+    scores[activePlayer] += currentScore
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer]
+
+    // 2. check if player's score is >= 100
+    if (scores[activePlayer] >= 100) {
+      // finish the game
+      playing = false
+      diceEl.classList.add('hidden')
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner')
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active')
+    } else {
+      // switch to next player
+      switchPlayer()
+    }
   }
 })
