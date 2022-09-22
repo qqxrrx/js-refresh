9793e9658b8d4051a1cfd89bc4cd3dfd7cd81bd0
ascendxrr <112617626+ascendxrr@users.noreply.github.com>
2022-09-22 13:48:02 +0800

    Sets (ES6)

---
+++ 04_data-structures_modern-operators_strings/script12.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const ordersSet = new Set([
+  'Pasta',
+  'Pizza',
+  'Pizza',
+  'Risotto',
+  'Pasta',
+  'Pizza',
+])
+
+console.log(ordersSet)
+
+console.log(new Set('John Doe'))
+
+console.log(ordersSet.size)
+
+console.log(ordersSet.has('Pizza'))
+console.log(ordersSet.has('Bread'))
+
+ordersSet.add('Garlic Bread')
+ordersSet.add('Garlic Bread')
+ordersSet.delete('Risotto')
+console.log(ordersSet)
+
+ordersSet.clear()
+console.log(ordersSet)
+
+for (const order of ordersSet) console.log(order)
+
+const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']
+
+// spread elements then store to array
+const staffUnique = [...new Set(staff)]
+
+console.log(staffUnique)
+console.log(
+  new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size
+)
+
+console.log(new Set('johndoe').size)
