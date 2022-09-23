028e8902226ded2a83aeea8f014e96d750ab432a
ascendxrr <112617626+ascendxrr@users.noreply.github.com>
2022-09-23 12:39:48 +0800

    Maps (ES6) - iteration

--- 04_data-structures_modern-operators_strings/script13.js
+++ 04_data-structures_modern-operators_strings/script13.js
@@ -1,5 +1,35 @@
 'use strict'
 
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
+
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0,
+    close: 23,
+  },
+  [`day-${2 + 4}`]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+}
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  openingHours,
+}
+
 const rest = new Map()
 rest.set('name', 'Classico Italiano')
 rest.set(1, 'Firenze, Italy')
@@ -41,3 +71,36 @@ console.log(rest)
 
 console.log(`[1, 2]`, rest.get([1, 2])) // undefined
 console.log(`using variable: `, rest.get(arr))
+
+// similar structure from Object.entries(), array of arrays
+const question = new Map([
+  ['question', 'What is the best ice cream?'],
+  [1, 'Chocolate'],
+  [2, 'Vanilla'],
+  [3, 'Double Dutch'],
+  ['correct', 3],
+  [true, 'Correct'],
+  [false, 'Try again'],
+])
+
+console.log(question)
+
+// convert object to map
+console.log(Object.entries(openingHours))
+const hoursMap = new Map(Object.entries(openingHours))
+console.log(hoursMap)
+
+// ------ quiz app -------
+console.log(question.get('question'))
+
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`)
+}
+
+const answer = Number(prompt('Your answer'))
+console.log(answer)
+console.log(question.get(answer === question.get('correct')))
+
+// convert map to array
+console.log([...question])
+console.log([...question.entries()])
